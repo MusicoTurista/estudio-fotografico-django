3f79ee9c943f22e85f9bf54e978e987357e20401
MusicoTurista <rafcascos@gmail.com>
2024-10-08 10:09:20 -0300

    Adição do CRUDE da classe Fotografo

--- sgf/aplic/admin.py
+++ sgf/aplic/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Fotografo)
+class FotografoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'telefone')
--- sgf/aplic/models.py
+++ sgf/aplic/models.py
@@ -7,3 +7,16 @@ def get_file_path(_instance, filename):
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
     return filename
+
+class Fotografo(models.Model):
+    cpf = models.IntegerField(_('CPF'), max_length=11)
+    telefone = models.IntegerField(_('Telefone'), max_length=15)
+    nome = models.CharField(_('Nome'), max_length=100)
+    sexo = models.CharField(_('Sexo'), max_length=20)
+    imagem = StdImageField(_('Foto'), null=True, blank=True, upload_to=get_file_path, variations={'thumb': {'width': 420, 'height': 260, 'crop': True}})
+    class Meta:
+        verbose_name = _('Fotografo')
+        verbose_name_plural = _('Fotografos')
+
+    def __str__(self):
+        return self.nome
