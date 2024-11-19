60225ead8fd0eff27ec977ffe246807b2a6b9b8b
MusicoTurista <rafcascos@gmail.com>
2024-11-19 12:24:30 -0300

    Implementação do sistema de login/logout

--- sgf/aplic/urls.py
+++ sgf/aplic/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('fotografo', FotografoView.as_view(), name='fotografo'),
     path('evento', EventoView.as_view(), name='evento'),
     path('evento-detalhe/<int:id>/', EventoDetalheView.as_view(), name='evento-detalhe'),
+
+    path('users/register', RegisterView, name='users/register'),
+    path('users/login', LoginView, name='users/login'),
+    path('users/logout', LogoutView, name='users/logout'),
 ]
--- sgf/aplic/views.py
+++ sgf/aplic/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+
+from django.contrib import messages
 # Create your views here.
 
 from django.views.generic import TemplateView
@@ -32,3 +36,37 @@ class EventoDetalheView(TemplateView):
         id = self.kwargs['id']
         context['evento'] = CategoriaEvento.objects.filter(id=id).first
         return context
+
+def RegisterView(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            #form.save()
+            messages.success(request, "Registrado com sucesso.")
+            return redirect('/users/login')
+    else:
+        form = UserCreationForm()
+    return render(request,"users/register.html",{'form':form})
+
+def LoginView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome back, {username}!")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def LogoutView(request):
+    logout(request)
+    return redirect('/users/login')
